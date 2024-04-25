35254a32fdd2ad936cb5dd0c5d5228a128ab2d48
Joakim Villo <villokodehode@gmail.com>
2024-04-25 11:09:17 +0200

    *

--- app/api/avatar/upload/route.ts
+++ app/api/avatar/upload/route.ts
@@ -5,22 +5,20 @@ export async function POST(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
   const filename = searchParams.get('filename');
 
-  // ⚠️ The below code is for App Router Route Handlers only
+  if (!filename) {
+    return new NextResponse('Filename is missing', { status: 400 });
+  }
+
+  if (!request.body) {
+    return new NextResponse('Request body is missing', { status: 400 });
+  }
+
   const blob = await put(filename, request.body, {
     access: 'public',
   });
 
-  // Here's the code for Pages API Routes:
-  // const blob = await put(filename, request, {
-  //   access: 'public',
-  // });
+
 
   return NextResponse.json(blob);
 }
 
-// The next lines are required for Pages API Routes only
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
